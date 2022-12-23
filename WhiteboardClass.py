5f2b9fa194440974f5a634de7108097d5dfd0028
Brongorg <brennan1207@gmail.com>
2022-12-23 11:37:47 -0600

    Version 0.21
    Made a class dedicated to holding functions used in the environment to clean up the run block

---
+++ WhiteboardClass.py
@@ -0,0 +1,33 @@
+import tkinter as  tk #import *
+from datetime import datetime
+
+window = tk.Tk() #Base root window that the whole project relies on. 
+def createWindow():
+    return tk.Toplevel() # creates a top window on top of lower window
+
+def createlabel(input): #Creates a label on the window with the text of the input variable converted to a string
+    return tk.Label(window, text = str(input))
+
+def createToolbarButton(buttonName,index): #creates a button with the names stored in an input array
+    prog_button = tk.Button(window, text = str(buttonName), padx = 50, pady = 20, command = useButton, bg = "grey")
+    prog_button.grid(row = 1, column = index)
+
+def useButton(): #Experiemental function to create a label
+    createlabel("button was clicked")
+    
+
+def completeButton(): #Use if the complete note button is pressed
+    return " "
+
+def createTextInput(): #Used to add text to the screen 
+    textLine = tk.Entry(window, width = 100, borderwidth=5, bg = "grey", fg = "black")
+    textLine.grid(row = 0, column = 0)
+    logInstance = datetime.now()
+    prt = logInstance.strftime("%m/%d/%y %H:%M:%S") + " : "
+    textLine.insert(0,prt)
+
+def textTranslate(entry): #Pulls the string from an entry on the window
+    return entry.get()
+
+def textPush(stringArray, entry): ##appends the text from the designated entry to the Master array
+    stringArray.append(textTranslate(entry))
